237d350db74a79874ea6f82c193ded96e63e1a95
JosephAMumford <JosephAMumford@gmail.com>
2018-09-16 23:02:01 -0500

    update

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRouting } from "./app.routing";
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouting],
+  imports: [BrowserModule, AppRouting, BrowserAnimationsModule, MaterialModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- angular-app/src/app/authorization/authorization.module.ts
+++ angular-app/src/app/authorization/authorization.module.ts
@@ -8,8 +8,10 @@ import { AuthorizationService } from "../authorization.service";
 import { LoginComponent } from "../login/login.component";
 import { RegisterComponent } from "../register/register.component";
 
+import { MaterialModule } from "../material/material.module";
+
 @NgModule({
-  imports: [CommonModule, FormsModule, AuthorizationRouting],
+  imports: [CommonModule, FormsModule, AuthorizationRouting, MaterialModule],
   declarations: [AuthorizationComponent, LoginComponent, RegisterComponent],
   exports: [AuthorizationComponent],
   providers: [AuthorizationService]
